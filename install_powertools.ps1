8beafac72a993b02b590cf258291bc98b083bfb0
Gerald Versluis <github@geraldversluis.nl>
2020-07-08 10:19:13 +0200

    Create install_powertools.ps1

---
+++ install_powertools.ps1
@@ -0,0 +1,4 @@
+$url2 = 'https://download.microsoft.com/download/D/B/E/DBE69906-B4DA-471C-8960-092AB955C681/powerapps-cli-1.0.msi'
+$output = 'C:/dotfiles/cli.msi'
+Start-BitsTransfer -Source $url2 -Destination $output
+Start-Process $output -ArgumentList '/quiet' -Wait
