b02232a2a9e9ce03d082e150dcc9ad442a3bb329
Gerald Versluis <github@geraldversluis.nl>
2020-07-09 13:45:33 +0200

    Install powerapps cli

--- install_powerapps.ps1
+++ install_powerapps.ps1
@@ -2,4 +2,5 @@ $url2 = 'https://download.microsoft.com/download/D/B/E/DBE69906-B4DA-471C-8960-0
 $output = 'C:/dotfiles/cli.msi'
 Start-BitsTransfer -Source $url2 -Destination $output
 Start-Process $output -ArgumentList '/quiet' -Wait
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
+
+Copy-Item profile.ps1 $profile
