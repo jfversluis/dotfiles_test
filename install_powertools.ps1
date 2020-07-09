fe4e9e860bc57cdbe71f2141444da16a7a6664bf
Gerald Versluis <github@geraldversluis.nl>
2020-07-09 11:30:30 +0200

    Update install_powertools.ps1

--- install_powertools.ps1
+++ install_powertools.ps1
@@ -2,3 +2,4 @@ $url2 = 'https://download.microsoft.com/download/D/B/E/DBE69906-B4DA-471C-8960-0
 $output = 'C:/dotfiles/cli.msi'
 Start-BitsTransfer -Source $url2 -Destination $output
 Start-Process $output -ArgumentList '/quiet' -Wait
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
