aa30d2a3e49074ba57f5b8c32f7c993b12a049b1
Gerald Versluis <github@geraldversluis.nl>
2020-07-22 14:58:01 +0200

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,3 +1,6 @@
+# Reload env variables because of the PowerApps CLI install
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+
 # Initialize git goodness
 Import-Module posh-git
 Import-Module oh-my-posh
