d76ed9804f1ef5046bac46979f0c2aabc8f6fd50
Gerald Versluis <github@geraldversluis.nl>
2020-07-22 13:44:28 +0200

    Add pretty git stuff

--- install.ps1
+++ install.ps1
@@ -1,7 +1,13 @@
+# Install PowerApps CLI
 $url2 = 'https://download.microsoft.com/download/D/B/E/DBE69906-B4DA-471C-8960-092AB955C681/powerapps-cli-1.0.msi'
 $output = 'C:/dotfiles/cli.msi'
 Start-BitsTransfer -Source $url2 -Destination $output
 Start-Process $output -ArgumentList '/quiet' -Wait
 
+# Install posh-git & oh-my-posh
+Install-Module posh-git -Scope CurrentUser -Confirm:$False -Force
+Install-Module oh-my-posh -Scope CurrentUser -Confirm:$False
+
+# Setup profile
 New-Item $profile -Force
 Copy-Item profile.ps1 $profile -Recurse -Force
