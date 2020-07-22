5e1fd910478a33644698c1e9c231d725e87daaac
Gerald Versluis <github@geraldversluis.nl>
2020-07-22 14:29:08 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -11,19 +11,19 @@ Install-Module oh-my-posh -Scope CurrentUser -Confirm:$False -Force
 # Install Cascadia Code
 # 1. Download Cascadia Code font from GitHub
 $DLPath = 'https://github.com/microsoft/cascadia-code/releases/download/v2007.01/CascadiaCode-2007.01.zip'
-$DLFile = 'C:\dotfiles\download\Cascadia.zip'
+$DLFile = 'C:\dotfiles\Cascadia.zip'
 Start-BitsTransfer -Source $DLPath -Destination $DLFile
 
 # 2. Unzip fonts
-expand-archive -path 'C:\dotfiles\download\Cascadia.zip' -destinationpath 'C:\dotfiles\download\unzipped'
+expand-archive -path 'C:\dotfiles\Cascadia.zip' -destinationpath 'C:\dotfiles'
 
 # 3. Now Install the Font 
 #$Font = New-Object -Com Shell.Application
 $Destination = (New-Object -ComObject Shell.Application).Namespace(0x14)
-$Destination.CopyHere('C:\dotfiles\download\unzipped\CascadiaCode.ttf',0x10)
-$Destination.CopyHere('C:\dotfiles\download\unzipped\CascadiaCodePL.ttf',0x10)
-$Destination.CopyHere('C:\dotfiles\download\unzipped\CascadiaMono.ttf',0x10)
-$Destination.CopyHere('C:\dotfiles\download\unzipped\CascadiaMonoPL.ttf',0x10)
+$Destination.CopyHere('C:\dotfiles\CascadiaCode.ttf',0x10)
+$Destination.CopyHere('C:\dotfiles\CascadiaCodePL.ttf',0x10)
+$Destination.CopyHere('C:\dotfiles\CascadiaMono.ttf',0x10)
+$Destination.CopyHere('C:\dotfiles\CascadiaMonoPL.ttf',0x10)
 
 if ((Test-Path -Path 'C:\workspace\.vscode' -PathType Container) -eq $False) {
     New-Item -Path 'C:\workspace\.vscode' -ItemType Directory
